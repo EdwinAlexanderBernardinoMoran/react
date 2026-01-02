2dbc4c5e322f99d0612e798b32dc164b89cb7082
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2026-01-01 18:50:24 -0600

    S3 - Variables

---
+++ reinforcement/src/01-var-const.ts
@@ -0,0 +1,10 @@
+const firstName: string = 'Edwin';
+let age: number = 30;
+age = 31;
+
+const isDeveloper: boolean = true;
+
+console.log({ firstName, age, isDeveloper });
+
+const containsLetterE = firstName.includes('d');
+console.log(`Does my name contain the letter 'd'? ${containsLetterE}`);
