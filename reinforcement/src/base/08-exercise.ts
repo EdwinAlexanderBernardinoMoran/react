1bf011f3bba8b280ebbe5d5482c3cd981c80a2c2
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2026-01-02 11:58:10 -0600

    S3 - Tarea - Desestructuracion

---
+++ reinforcement/src/base/08-exercise.ts
@@ -0,0 +1,11 @@
+const useState = (name: string) => {
+    return [name, (newName: string) => {
+        console.log(newName);
+        
+    }] as const;
+}
+
+const [name, setName] = useState('Goku');
+
+console.log(name);
+setName('Vegeta');
