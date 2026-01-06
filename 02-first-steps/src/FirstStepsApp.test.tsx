3b90577c0349876254db13963e13ab435fac267d
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2026-01-06 14:29:05 -0600

    s5 - Componentes ficticios - Mock Componentes

---
+++ 02-first-steps/src/FirstStepsApp.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+import { FirstStepsApp } from "./FirstStepsApp";
+
+vi.mock('./shopping-cart/ItemCounter', () => ({
+    ItemCounter: () => <div data-testid="ItemCounter"/>
+}))
+
+describe('FirstStepsApp', () => {
+    test('should match snapshot', () => {
+        const { container } = render(<FirstStepsApp/>);
+
+        expect(container).toMatchSnapshot();
+    });
+
+    test('should render the correct number of ItemCounter components', () => {
+        render(<FirstStepsApp/>);
+
+        const itemCounters = screen.getAllByTestId('ItemCounter');
+        console.log(itemCounters.length);
+
+        expect(itemCounters.length).toBe(3);
+    });
+});
