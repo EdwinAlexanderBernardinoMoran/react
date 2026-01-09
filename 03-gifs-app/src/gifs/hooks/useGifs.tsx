14608300dd1586c90337146cebd3552aa3dcb3a8
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2026-01-08 22:17:35 -0600

    s7 - manejo en cache

--- 03-gifs-app/src/gifs/hooks/useGifs.tsx
+++ 03-gifs-app/src/gifs/hooks/useGifs.tsx
@@ -2,12 +2,21 @@ import { useState } from "react"
 import type { Gif } from "../interfaces/gif.interface"
 import { getGifsByQuery } from "../actions/get-gifs-by-query.action"
 
+const gifsCache: Record<string, Gif[]> = {}
+
 export const useGifs = () => {
     const [previousTems, setPreviousTems] = useState<string[]>([])
     const [gifs, setGifs] = useState<Gif[]>([])
-    
-    const handleTermClick = (term: string) => {
-        console.log(`Term clicked: ${term}`)
+
+    const handleTermClick = async (term: string) => {
+
+        if (gifsCache[term]) {
+            setGifs(gifsCache[term]);
+            return;
+        }
+
+        const gifs = await getGifsByQuery(term);
+        setGifs(gifs)
     }
     
     const handleOnSearch = async (query: string = '') => {
@@ -20,6 +29,12 @@ export const useGifs = () => {
     
         const gifsItems = await getGifsByQuery(query);
         setGifs(gifsItems)
+
+        
+        console.log(gifsItems);
+        
+        gifsCache[query] = gifsItems
+        console.log(gifsCache);
     }
 
     return {
