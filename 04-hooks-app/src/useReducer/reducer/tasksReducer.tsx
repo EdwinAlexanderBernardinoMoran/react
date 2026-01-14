7f4d1f18448e800f9daaa6e2145ec7bf66d54b89
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2026-01-13 22:22:50 -0600

    s8 - configuracion de la funcion reducer

--- 04-hooks-app/src/useReducer/reducer/tasksReducer.tsx
+++ 04-hooks-app/src/useReducer/reducer/tasksReducer.tsx
@@ -17,5 +17,53 @@ export type TaskAction =
 | {type: 'DELETE_TODO', payload: number}
 
 export const tasksReducer = (state: TaskState, action: TaskAction): TaskState => {
-  return state;
+    switch (action.type) {
+        case 'ADD_TODO': {
+            const newTodo: Todo = {
+                id: Date.now(),
+                text: action.payload.trim(),
+                completed: false,
+            }
+            return {
+                ...state,
+                todos: [...state.todos, newTodo],
+                length: state.todos.length,
+                pending: state.pending + 1,
+            };
+        }
+        case 'TOOGLE_TODO': {
+
+            // Crear un nuevo array con el todo actualizado
+            const updatadTodos = state.todos.map( todo => {
+                if ( todo.id === action.payload) {
+                    return { ...todo, completed: !todo.completed }
+                }
+                return todo;
+            })
+
+            return {
+                ...state,
+                todos: updatadTodos,
+                length: state.todos.length,
+                completed: updatadTodos.filter(todo => todo.completed).length,
+                pending: updatadTodos.filter(todo => !todo.completed).length,
+            }
+        }
+        case 'DELETE_TODO': {
+            const currentTodos = state.todos.filter(
+                (todo) => todo.id !== action.payload
+            );
+
+            return {
+                ...state,
+                todos: currentTodos,
+                length: currentTodos.length,
+                completed: currentTodos.filter(todo => todo.completed).length,
+                pending: currentTodos.filter(todo => !todo.completed).length,
+            };
+        }
+        
+        default:
+            return state;
+    }
 }
