8a6ab6cae002f82b3b3cd1d63304636c1d675a7e
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2026-01-15 06:38:35 -0600

    Actualizando README

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster, toast } from 'sonner'
 
 // import { HooksApp } from './HooksApp'
 
@@ -16,6 +17,7 @@ import { InstagromApp } from './useOptimistic/InstagromApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster/>
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
