c19dddddf1ec0ee1e8b7a1227e6c2b82419aa642
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2026-01-14 23:25:42 -0600

    s8 - Efectos secundarios con un reducer

--- 04-hooks-app/src/useReducer/ScrambleWords.tsx
+++ 04-hooks-app/src/useReducer/ScrambleWords.tsx
@@ -2,7 +2,7 @@
 // Es necesario componentes de Shadcn/ui
 // https://ui.shadcn.com/docs/installation/vite
 
-import React, {useReducer } from 'react';
+import React, {useEffect, useReducer } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Card, CardContent } from '@/components/ui/card';
@@ -16,6 +16,17 @@ export const ScrambleWords = () => {
   const [state, dispatch] = useReducer(scrambleWordsReducer, getInitialState())
 
   const {currentWord, errorCounter, guess, isGameOver, maxAllowErrors, maxSkips, points, scrambledWord, skipCounter, words, totalWords} = state;
+
+  useEffect(() => {
+
+    if (points === 0) return;
+
+    confetti({
+      particleCount: 100,
+      spread: 120,
+      origin: {y: 0.6}
+    })
+  }, [points])
 
   const handleGuessSubmit = (e: React.FormEvent) => {
     // Previene el refresh de la página
