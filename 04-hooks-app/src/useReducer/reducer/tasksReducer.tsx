e5c48d7d199f9039b7e1468734e8bbfb412a2f11
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2026-01-14 20:56:59 -0600

    s8 - Persistencia local - LocalStoraged, Validadores de objetos - instalacion de zod

--- 04-hooks-app/src/useReducer/reducer/tasksReducer.tsx
+++ 04-hooks-app/src/useReducer/reducer/tasksReducer.tsx
@@ -1,3 +1,5 @@
+import * as z from "zod";
+
 interface Todo {
     id: number;
     text: string;
@@ -16,13 +18,45 @@ export type TaskAction =
 | {type: 'TOGGLE_TODO', payload: number}
 | {type: 'DELETE_TODO', payload: number}
 
+const TodoSchema = z.object({
+    id: z.number(),
+    text: z.string(),
+    completed: z.boolean(),
+})
+
+const TaskStateSchema = z.object({
+    todos: z.array(TodoSchema),
+    length: z.number(),
+    completed: z.number(),
+    pending: z.number(),
+})
+
 export const getTasksInitialState = (): TaskState => {
-    return {
-        todos: [],
-        length: 0,
-        completed: 0,
-        pending: 0,
+
+    const localStorageState = localStorage.getItem('task-state');
+    if (!localStorageState) {
+        return {
+            todos: [],
+            length: 0,
+            completed: 0,
+            pending: 0,
+        }
     }
+
+    // Validar mediante Zod
+    const result = TaskStateSchema.safeParse(JSON.parse(localStorageState));
+
+    if (result.error){
+        console.log(result.error);   
+        return {
+            todos: [],
+            length: 0,
+            completed: 0,
+            pending: 0,
+        }
+    }
+    
+    return result.data;
 }
 
 export const tasksReducer = (state: TaskState, action: TaskAction): TaskState => {
