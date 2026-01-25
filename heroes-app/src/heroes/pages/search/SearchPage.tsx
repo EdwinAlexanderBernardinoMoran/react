5bdac68d5cd51cc36076130090bf979d73cc3095
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2026-01-24 21:30:14 -0600

    s11 - Componentes de busqueda

--- heroes-app/src/heroes/pages/search/SearchPage.tsx
+++ heroes-app/src/heroes/pages/search/SearchPage.tsx
@@ -1,12 +1,17 @@
 import { HeroStats } from '@/heroes/components/HeroStats';
 import { CustomJumbotron } from '../../../components/custom/CustomJumbotron';
+import { SearchControls } from './ui/SearchControls';
 
 export const SearchPage = () => {
   return (
     <>
       <CustomJumbotron title="Search Heroes" subtitle="Find your favorite heroes quickly and easily." />
 
+      {/* Stats Dashboard */}
       <HeroStats />
+
+      {/* Search Controls */}
+      <SearchControls />
     </>
   )
 }
