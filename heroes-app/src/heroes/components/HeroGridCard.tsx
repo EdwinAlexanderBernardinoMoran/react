432d5d3d3217b73e377a6f198383ff31f2791dd7
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2026-02-06 21:53:53 -0600

    s12 - Navegacion manual dentro de las tarjetas

--- heroes-app/src/heroes/components/HeroGridCard.tsx
+++ heroes-app/src/heroes/components/HeroGridCard.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router";
 import { Brain, Eye, Gauge, Heart, Shield, Zap } from "lucide-react";
 
 import { Badge } from "@/components/ui/badge";
@@ -11,14 +12,22 @@ interface HeroGridCardProps {
 }
 
 export const HeroGridCard = ({ hero }: HeroGridCardProps) => {
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/heroes/${hero.slug}`);
+  }
+
   return (
     <>
       <Card className="group overflow-hidden hover:shadow-xl transition-all duration-300 hover:-translate-y-1 bg-linear-to-br from-white to-gray-50">
-        <div className="relative h-64 overflow-hidden">
+        <div className="relative h-64">
           <img
             src={hero.image}
             alt={hero.name}
-            className="object-cover transition-all duration-500 group-hover:scale-110"
+            className="object-cover transition-all duration-500 group-hover:scale-110 absolute -top-7 w-full h-102.5"
+            onClick={handleClick}
           />
 
           {/* Status indicator */}
@@ -58,7 +67,7 @@ export const HeroGridCard = ({ hero }: HeroGridCardProps) => {
           </Button>
         </div>
 
-        <CardHeader className="pb-3">
+        <CardHeader className="py-3 z-10 bg-gray-100/50 backdrop-blur-sm relative top-1 group-hover:top-2.5 transition-all duration-300">
           <div className="flex justify-between items-start">
             <div className="space-y-1">
               <h3 className="font-bold text-lg leading-tight">{hero.alias}</h3>
@@ -125,7 +134,7 @@ export const HeroGridCard = ({ hero }: HeroGridCardProps) => {
 
               {
                 hero.powers.slice(0, 3).map((power) => (
-                  <Badge variant="outline" className="text-xs">
+                  <Badge variant="outline" className="text-xs" key={power}>
                     {power}
                   </Badge>
                 ))
--- heroes-app/src/router/app.router.tsx
+++ heroes-app/src/router/app.router.tsx
@@ -3,7 +3,7 @@ import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
 import { HomePage } from "@/heroes/pages/home/HomePage";
 import { lazy } from "react";
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 
 const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'))
 const AdminPage = lazy(() => import('@/admin/pages/AdminPage'))
@@ -18,13 +18,17 @@ export const appRouter = createBrowserRouter([
                 element: <HomePage />
             },
             {
-                path: 'heroes/1',
+                path: 'heroes/:idSlug',
                 element: <HeroPage />
             },
             {
                 path: 'search',
                 element: <SearchPage />
             },
+            {
+                path: '*',
+                element: <Navigate to='/' replace /> // Redirect any unknown routes to home
+            }
         ]
     },
     {
